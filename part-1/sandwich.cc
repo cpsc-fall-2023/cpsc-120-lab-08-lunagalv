f53542b06471a2537e98ef2ad60c60137126f59c
Luna Galvan <lulegalvan@csu.fullerton.edu>
2023-11-01 21:35:26 -0700

    fixed formatting

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -10,25 +10,16 @@
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
 
-  // Done: Validate that the number of arguments is correct.
-  // If not, print an error message and return a non-zero value.
   if (arguments.size() != 4) {
-    std::cout << "error: you must supply three arguments \n";
+    std::cout << "error: you must supply three arguments";
     return 1;
-}   else if (arguments.size() == 4) {
-    std::string protein{arguments[1]};
-    std::string bread{arguments[2]};
-    std::string condiment{arguments[3]};
-    std::cout << "Your order: A " << protein << " sandwich on " << bread
-            << " with " << condiment << ".\n";
-    return 0;
   }
-}
-  // DONE: Declare three std::string variables to hold the
-  // protein, bread, and condiment input.
-  // Initialize each variable with an element of the arguments vector
-  // declared above.
+  std::string protein = arguments.at(1);
+  std::string bread = arguments.at(2);
+  std::string condiment = arguments.at(3);
+  std::cout << "Your order:\n"
+            << "A " << protein << " sandwich on " << bread << " with "
+            << condiment;
 
-  // DONE: Use cout to print output following the pattern
-  // Your order: A *PROTEIN* sandwich on *BREAD* with *CONDIMENT*.
-  // on its own line.
+  return 0;
+};
